142e8d48f5b637eed8a2449834a8cc2603a56e53
Jason Hu <garbhj6@gmail.com>
2026-06-28 14:12:57 -0600

    gpt2 pt. 2.3 - weight init fixed

--- gpt2/train_gpt2.py
+++ gpt2/train_gpt2.py
@@ -33,6 +33,7 @@ class CausalSelfAttention(nn.Module):
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
         # regularization
         self.n_head = config.n_head
         self.n_embd = config.n_embd
@@ -69,6 +70,7 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu = nn.GELU(approximate='tanh') 
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
 
     def forward(self, x):
         x = self.c_fc(x)
@@ -114,6 +116,23 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embd),
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+        # Weight sharing scheme
+        self.transformer.wte.weight = self.lm_head.weight
+
+        # Parameter init
+        self.apply(self._init_weights)  # torch.nn.Module.apply(fn) to all submodules
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, "NANOGPT_SCALE_INIT"):
+                std *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        if isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
     def forward(self, idx, targets=None):
         # idx is of shape (B, T)
@@ -185,29 +204,61 @@ class GPT(nn.Module):
         return model
 
 # ---------------------------------------------------------------------------
+import tiktoken
+
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        # At init load tokens from disk and store them in memory
+        with open('input.txt', 'r') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding('gpt2')
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+        # State
+        self.current_position = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position+B*T+1]
+        x = (buf[:-1]).view(B, T) # inputs
+        y = (buf[1:]).view(B, T) # targets
+        # Advance the position in the tensor
+        self.current_position += B * T
+        # If loading the next batch would be out of bounds, reset
+        if self.current_position + (B * T + 1) > len(self.tokens):
+            self.current_position = 0
+        return x, y
+
+
+
+# ---------------------------------------------------------------------------
+
 device = 'cpu'  # Because mps seems to have issue with optimizing with zero gradients
 print("using device:", device)
 
-# Get a data batch
-import tiktoken
-enc = tiktoken.get_encoding('gpt2')
-with open('input.txt', 'r') as f:
-    text = f.read()
-text = text[:1000]
-tokens = enc.encode(text)
-B, T = 4, 32
-buf = torch.tensor(tokens[:B*T + 1])  # +1 for offset of y
-buf = buf.to(device)
-x = buf[:-1].view(B, T)  # (B, T)
-y = buf[1:].view(B, T)  # (B, T)
+torch.manual_seed(1337)
+if torch.mps.is_available():
+    torch.mps.manual_seed(1337)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(1337)
 
-# get logits
+train_loader = DataLoaderLite(B=4, T=32)
+
+# Initialize Model
 model = GPT(GPTConfig())  # initialize new model
 model.to(device)
 
 # Optimize:
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
 for i in range(50):
+    x, y = train_loader.next_batch()
+    x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
     logits, loss = model(x, y) 
     loss.backward()
@@ -215,6 +266,8 @@ for i in range(50):
     print(f"step {i}, loss: {loss.item()}")
 
 import sys; sys.exit(0)
+
+# ---------------------------------------------------------------------------
 
 
 # Evaluate model
